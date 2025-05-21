8fc255c47ae1af62daec5701ee935ff5d3616c91
thamaraikannan <thamaraik69@gmail.com>
2025-05-21 20:29:20 +0530

    images updated

--- src/component/Product/index.jsx
+++ src/component/Product/index.jsx
@@ -4,17 +4,23 @@ export default function Products() {
   return (
     <>
       <div className="max-w-[992px] m-auto py-24">
-        <p className="text-[22px]">Guppies (Premium Strains)</p>
-        <div className="flex gap-6 mt-10">
-          {fish.map((img, index) => (
-            <>
-              <div key={index} className="relative">
-                <img src={img.img} className="w-[100%] block rounded-2xl"   />
-                <p className="absolute top-[50%] text-center py-2  z-50 text-[20px] fish_name text-white w-[100%] font-semibold">{img.name}</p>
-              </div>
-            </>
-          ))}
-        </div>
+        {fish.map((item) => (
+          <>
+            <p className="text-[38px] font-semibold">{item.title}</p>
+            <div className="flex gap-6 mt-10">
+              {item.content.map((img, index) => (
+                <>
+                  <div key={index} className="relative">
+                    <img src={img.img} className="w-[100%] block rounded-2xl" />
+                    <p className="absolute top-[50%] text-center py-2  z-50 text-[20px] fish_name text-white w-[100%] font-semibold">
+                      {img.name}
+                    </p>
+                  </div>
+                </>
+              ))}
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
