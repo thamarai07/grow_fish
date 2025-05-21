0bfbf1f682c5289a1f8d77e5f0a129a4695f2249
thamaraikannan <thamaraik69@gmail.com>
2025-05-21 20:47:38 +0530

    create product

--- src/component/Product/index.jsx
+++ src/component/Product/index.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { fish } from "@/utils/content";
+import Link from "next/link";
+
 export default function Products() {
   return (
-    <>
-      <div className="max-w-[992px] m-auto py-24">
-        {fish.map((item) => (
-          <>
-            <p className="text-[38px] font-semibold">{item.title}</p>
-            <div className="flex gap-6 mt-10">
-              {item.content.map((img, index) => (
-                <>
-                  <div key={index} className="relative">
-                    <img src={img.img} className="w-[100%] block rounded-2xl" />
-                    <p className="absolute top-[50%] text-center py-2  z-50 text-[20px] fish_name text-white w-[100%] font-semibold">
-                      {img.name}
-                    </p>
-                  </div>
-                </>
-              ))}
-            </div>
-          </>
-        ))}
-      </div>
-    </>
+    <div className="max-w-[992px] m-auto py-24">
+      {fish.map((item, i) => (
+        <div key={i}>
+          <p className="text-[38px] font-semibold">{item.title}</p>
+          <div className="flex gap-6 mt-10">
+            {item.content.map((img, index) => (
+              <div key={index} className="relative">
+                <Link href={`/product/${img.id}`}>
+                  <img
+                    src={img.img}
+                    alt={img.name}
+                    className="w-full block rounded-2xl"
+                  />
+                  <p className="absolute top-[50%] text-center py-2 z-50 text-[20px] fish_name text-white w-full font-semibold">
+                    {img.name}
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
